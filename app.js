01289efc53feed89a2d21070aeab482fda149dcc
Diana Maliukh <d.maliukh@spd-ukraine.com>
2021-10-28 11:59:41 +0300

    add new response codes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const expressValidation = require("express-validation");
+var expressValidation = require("express-validation");
 
-const RESPONSE_CODES = require("./responseCodes")
+var RESPONSE_CODE = require("./responseCodes");
 
-function responseMiddleware(error, req, res, next) {
+function responseHandler(error, req, res, next) {
     try {
-        let dataToSent = {
+        var dataToSent = {
             timestamp: Date.now(),
-            status: error.status,
+            status: 500,
         };
 
         if (error instanceof expressValidation.ValidationError) {
@@ -17,32 +17,80 @@ function responseMiddleware(error, req, res, next) {
                 .join("/n");
 
             Object.assign(dataToSent, {
+                statusCode: RESPONSE_CODE.REQ_VALID_ERROR,
                 status: 400,
-                statusCode: RESPONSE_CODES.REQ_VALID_ERROR,
                 message: createdMessage,
                 data: null,
             });
         }
 
         switch (error.responseCode) {
-            case RESPONSE_CODES.PROCESS_ERROR: {
+            case RESPONSE_CODE.P_ERROR__FORBIDDEN: {
                 Object.assign(dataToSent, {
+                    errorCode: RESPONSE_CODE.P_ERROR__FORBIDDEN,
+                    status: 403,
                     message: error.data,
-                    errorCode: RESPONSE_CODES.PROCESS_ERROR,
                     data: null,
                 });
                 break;
             }
-            case RESPONSE_CODES.BASIC_SUCCESS: {
+            case RESPONSE_CODE.P_ERROR__NOT_FOUND: {
                 Object.assign(dataToSent, {
+                    errorCode: RESPONSE_CODE.P_ERROR__NOT_FOUND,
+                    status: 404,
+                    message: error.data,
+                    data: null,
+                });
+                break;
+            }
+            case RESPONSE_CODE.P_ERROR__UNAUTHORIZED: {
+                Object.assign(dataToSent, {
+                    errorCode: RESPONSE_CODE.P_ERROR__UNAUTHORIZED,
+                    status: 401,
+                    message: error.data,
+                    data: null,
+                });
+                break;
+            }
+            case RESPONSE_CODE.S_ERROR_INTERNAL: {
+                Object.assign(dataToSent, {
+                    errorCode: RESPONSE_CODE.S_ERROR_INTERNAL,
+                    status: 500,
+                    message: error.data,
+                    data: null,
+                });
+                break;
+            }
+            case RESPONSE_CODE.BASIC_SUCCESS: {
+                Object.assign(dataToSent, {
+                    status: 200,
                     message: error.data,
                     errorCode: null,
                     data: null,
                 });
                 break;
             }
-            case RESPONSE_CODES.SUCCESS: {
+            case RESPONSE_CODE.BASIC_SUCCESS__CREATED: {
                 Object.assign(dataToSent, {
+                    status: 201,
+                    message: error.data,
+                    errorCode: null,
+                    data: null,
+                });
+                break;
+            }
+            case RESPONSE_CODE.SUCCESS: {
+                Object.assign(dataToSent, {
+                    status: 200,
+                    message: error.data.message,
+                    errorCode: null,
+                    data: error.data.data,
+                });
+                break;
+            }
+            case RESPONSE_CODE.SUCCESS__CREATED: {
+                Object.assign(dataToSent, {
+                    status: 201,
                     message: error.data.message,
                     errorCode: null,
                     data: error.data.data,
@@ -53,6 +101,10 @@ function responseMiddleware(error, req, res, next) {
         return res.status(dataToSent.status).json(dataToSent);
     } catch (e) {
         console.log("RESPONSE_HANDLER_CATCH", e);
+        res.status(500).json(e.message);
     }
 }
-module.exports = { sendResponse: responseMiddleware, RESPONSE_CODES };
+
+module.exports = {
+    sendResponse: responseHandler,
+};
